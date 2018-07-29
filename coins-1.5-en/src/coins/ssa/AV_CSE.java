ecb9c90d92a87b126540433b7c3c76eae806131f
Naoya Niwa <mine@mine-studio.com>
2018-07-30 03:20:34 +0900

    Add expmap

--- coins-1.5-en/src/coins/ssa/AV_CSE.java
+++ coins-1.5-en/src/coins/ssa/AV_CSE.java
@@ -15,7 +15,9 @@ import coins.backend.sym.Symbol;
 
 import coins.backend.util.*;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Stack;
 import java.util.Vector;
 
@@ -51,11 +53,8 @@ public class AV_CSE implements LocalTransformer {
 	// Util includes convenient methods
 	private Util util;
 
-	// The variable is used for determining a bit location of each expression on
-	// For getting the bit location corresponding to a LirNode
-	Hashtable<LirNode, Integer> exp2id;
-	// For getting the LirNode corresponding to a bit location
-	private Vector<LirNode> id2exp;
+	private ExpMap expMap;
+
 	// For recording MEM nodes
 	private Vector<Integer> memq;
 
@@ -76,13 +75,18 @@ public class AV_CSE implements LocalTransformer {
 	// Used as a worklist
 	private Stack<BasicBlk> stack;
 
+	boolean IsTargetExpression(LirNode exp) {
+		return exp.opCode != Op.INTCONST && exp.opCode != Op.FLOATCONST && exp.opCode != Op.REG && exp.opCode != Op.CALL
+				&& exp.opCode != Op.USE && exp.opCode != Op.CLOBBER;
+	}
+
 	// Performing the followings while searching all the expressions:
 	// 1. determining a bit location of each expression,
 	// and recording an association between the expression and the bit location in
 	// id2exp and exp2id respectively.
 	// 2. splitting x = e into t = e; x = t introducing a temporary t, which is
 	// recorded in tempPool.
-	void collectExp() {
+	void collectExp() throws Exception {
 		for (BiLink bbl = f.flowGraph().basicBlkList.first(); !bbl.atEnd(); bbl = bbl.next()) {
 			BasicBlk bb = (BasicBlk) bbl.elem();
 
@@ -92,23 +96,19 @@ public class AV_CSE implements LocalTransformer {
 				// Our target expression if the right hand side of each assignment statement
 				// such as
 				// (SET (...) e)
-				if (node.opCode == Op.SET) {
-					if (node.kid(0).opCode == Op.REG && node.kid(1).opCode != Op.INTCONST
-							&& node.kid(1).opCode != Op.FLOATCONST && node.kid(1).opCode != Op.REG
-							&& node.kid(1).opCode != Op.CALL && node.kid(1).opCode != Op.USE
-							&& node.kid(1).opCode != Op.CLOBBER) {
+				if (node.opCode == Op.SET && node.kid(0).opCode == Op.REG) {
+					LirNode exp = node.kid(1);
+					// X <- A * B
+					if (IsTargetExpression(exp)) {
 						// The right-hand side expression of node is used as a key of some tables.
-						LirNode exp = node.kid(1);
 
 						// Getting a bit location from an expression. If it has not been set yet, set
 						// it.
-						Integer id = (Integer) exp2id.get(exp);
+						Integer id = expMap.getId(exp);
 						if (id == null) {
-							id = new Integer(id2exp.size());
-							exp2id.put(exp, id);
-							id2exp.add(exp);
+							id = expMap.AddExp(exp);
 
-							// Generateing the temporary symbol for expression exp through the newSsaSymbol
+							// Generating the temporary symbol for expression exp through the newSsaSymbol
 							// of SsaSymTab, and
 							// record it in tmpPool. Symbol is a kind of string, but it is much faster to
 							// compare the Symbols.
@@ -150,11 +150,9 @@ public class AV_CSE implements LocalTransformer {
 				// (SET (REG ...) (...)):
 				if (node.kid(0).opCode == Op.REG) {
 					boolean isRemoved = false;
-					if (node.kid(1).opCode != Op.INTCONST && node.kid(1).opCode != Op.FLOATCONST
-							&& node.kid(1).opCode != Op.REG && node.kid(1).opCode != Op.CALL
-							&& node.kid(1).opCode != Op.USE && node.kid(1).opCode != Op.CLOBBER) {
-						LirNode exp = node.kid(1);
-						Integer id = (Integer) exp2id.get(exp);
+					LirNode exp = node.kid(1);
+					if (IsTargetExpression(exp)) {
+						Integer id = expMap.getId(exp);
 						int index = id.intValue();
 
 						// If gen has been set already,
@@ -169,16 +167,17 @@ public class AV_CSE implements LocalTransformer {
 					}
 					// Search expressions including a variable assigned by the node,
 					// and then, set kills of the expressions, and reset gens of them
-					for (int i = 0; !isRemoved && i < id2exp.size(); i++) {
-						LirNode exp = id2exp.elementAt(i);
-
-						BiList operands = util.findTargetLir(exp, Op.REG, new BiList());
-						for (BiLink q = operands.first(); !q.atEnd(); q = q.next()) {
-							LirNode reg = (LirNode) q.elem();
-							Symbol regSym = ((LirSymRef) reg).symbol;
-							if (regSym == ((LirSymRef) node.kid(0)).symbol) {
-								kill.setBit(i);
-								gen.resetBit(i);
+					if (!isRemoved) {
+						for (int i = 0; i < expMap.getSize(); i++) {
+							LirNode exp1 = expMap.getById(i);
+							BiList operands = util.findTargetLir(exp1, Op.REG, new BiList());
+							for (BiLink q = operands.first(); !q.atEnd(); q = q.next()) {
+								LirNode reg = (LirNode) q.elem();
+								Symbol regSym = ((LirSymRef) reg).symbol;
+								if (regSym == ((LirSymRef) node.kid(0)).symbol) {
+									kill.setBit(i);
+									gen.resetBit(i);
+								}
 							}
 						}
 					}
@@ -208,8 +207,8 @@ public class AV_CSE implements LocalTransformer {
 				if (node.kid(2).nKids() > 0 && node.kid(2).kid(0).opCode == Op.REG) {
 					Symbol returnS = ((LirSymRef) node.kid(2).kid(0)).symbol;
 
-					for (int i = 0; i < id2exp.size(); i++) {
-						LirNode exp = id2exp.elementAt(i);
+					for (int i = 0; i < expMap.getSize(); i++) {
+						LirNode exp = expMap.getById(i);
 						BiList operands = util.findTargetLir(exp, Op.REG, new BiList());
 						for (BiLink q = operands.first(); !q.atEnd(); q = q.next()) {
 							LirNode reg = (LirNode) q.elem();
@@ -227,9 +226,7 @@ public class AV_CSE implements LocalTransformer {
 
 	void init() {
 		for (BiLink bb = f.flowGraph().basicBlkList.first(); !bb.atEnd(); bb = bb.next()) {
-
 			BasicBlk v = (BasicBlk) bb.elem();
-
 			initGenKill(v, gen[v.id], kill[v.id]);
 
 			// Set all bits of "in" for each basic block except a start node.
@@ -241,7 +238,7 @@ public class AV_CSE implements LocalTransformer {
 			gen[v.id].vectorOr(out[v.id], out[v.id]);
 
 			// If "out" includes bit value 0, push it on stack.
-			BitVector notOut = new BitVector(id2exp.size());
+			BitVector notOut = new BitVector(expMap.getSize());
 			out[v.id].vectorNot(notOut);
 			if (!notOut.isEmpty()) {
 				stack.push(v);
@@ -267,7 +264,7 @@ public class AV_CSE implements LocalTransformer {
 				// in[succ.id].vectorAnd(out[v.id], in[succ.id]);
 
 				// Calculate "out" from "in" of the successor
-				BitVector newOut = new BitVector(id2exp.size());
+				BitVector newOut = new BitVector(expMap.getSize());
 				in[succ.id].vectorSub(kill[succ.id], newOut);
 				newOut.vectorOr(gen[succ.id], newOut);
 				// If any changes exists in "out",
@@ -286,7 +283,7 @@ public class AV_CSE implements LocalTransformer {
 		for (BiLink bb = f.flowGraph().basicBlkList.first(); !bb.atEnd(); bb = bb.next()) {
 			BasicBlk v = (BasicBlk) bb.elem();
 
-			initGenKill(v, in[v.id], new BitVector(id2exp.size()));
+			initGenKill(v, in[v.id], new BitVector(expMap.getSize()));
 		}
 	}
 
@@ -296,25 +293,27 @@ public class AV_CSE implements LocalTransformer {
 		util = new Util(env, f);
 
 		stack = new Stack<BasicBlk>();
-		exp2id = new Hashtable<LirNode, Integer>();
-		id2exp = new Vector<LirNode>();
+		expMap = new ExpMap();
 		memq = new Vector<Integer>();
 		tmpPool = new Vector<Symbol>();
+
+		try {
+			collectExp();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 
 		int idBound = f.flowGraph().idBound();
 		kill = new BitVector[idBound];
 		gen = new BitVector[idBound];
 		in = new BitVector[idBound];
 		out = new BitVector[idBound];
-		;
-
-		collectExp();
-
 		for (int i = 0; i < f.flowGraph().idBound(); i++) {
-			gen[i] = new BitVector(id2exp.size());
-			kill[i] = new BitVector(id2exp.size());
-			in[i] = new BitVector(id2exp.size());
-			out[i] = new BitVector(id2exp.size());
+			gen[i] = new BitVector(expMap.getSize());
+			kill[i] = new BitVector(expMap.getSize());
+			in[i] = new BitVector(expMap.getSize());
+			out[i] = new BitVector(expMap.getSize());
 		}
 
 		init();
@@ -323,5 +322,41 @@ public class AV_CSE implements LocalTransformer {
 
 		f.flowGraph().touch();
 		return (true);
+	}
+}
+
+class ExpMap {
+	// The variable is used for determining a bit location of each expression on
+	// For getting the bit location corresponding to a LirNode
+	Hashtable<LirNode, Integer> exp2id;
+	// For getting the LirNode corresponding to a bit location
+	private List<LirNode> id2exp;
+
+	public ExpMap() {
+		exp2id = new Hashtable<LirNode, Integer>();
+		id2exp = new ArrayList<LirNode>();
+	}
+
+	Integer getId(LirNode exp) {
+		return (Integer) exp2id.get(exp);
+	}
+
+	Integer AddExp(LirNode exp) throws Exception {
+		if (getId(exp) != null) {
+			throw new Exception();
+		} else {
+			Integer id = new Integer(id2exp.size());
+			exp2id.put(exp, id);
+			id2exp.add(exp);
+			return id;
+		}
+	}
+
+	int getSize() {
+		return id2exp.size();
+	}
+
+	LirNode getById(int id) {
+		return id2exp.get(id);
 	}
 }
